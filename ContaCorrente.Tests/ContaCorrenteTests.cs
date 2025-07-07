d3f4e467fbcb7aa2572f750faaf7cbc1e8856882
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-07-07 00:19:44 -0300

    Test conta: cria teste unitário para InativarContaAsync() com Xunit.

---
+++ ContaCorrente.Tests/ContaCorrenteTests.cs
@@ -0,0 +1,28 @@
+﻿using ContaCorrente.Domain.Entities;
+using Xunit;
+
+namespace ContaCorrente.Tests.Domain
+{
+    public class ContaCorrenteTests
+    {
+        [Fact]
+        public void Inativar_DeveAlterarStatusParaInativo()
+        {
+            // Arrange
+            var conta = new ContaCorrente.Domain.Entities.ContaCorrente(
+                Guid.NewGuid(),
+                cpf: "12345678901",
+                nome: "Gustavo",
+                numero: 1234,
+                senha: "hash",
+                salt: "salt"
+            );
+
+            // Act
+            conta.Inativar();
+
+            // Assert
+            Assert.False(conta.Ativo);
+        }
+    }
+}
